c3c79145f39773b207c48c7b5eb6f2b659262563
Ross Cunningham <Ross.Cunningham@dyson.com>
2019-04-03 16:05:10 +0100

    Added remaining quaternion operators. Added lerp functions for quaternion

--- lib/math/quat.cpp
+++ lib/math/quat.cpp
@@ -21,7 +21,7 @@ z(z),
 w(w)
 {}
 
-Quat::Quat(const Vec3& axis, const Angle& angle)
+Quat::Quat(const Quat& axis, const Angle& angle)
 {
     const float sin_half = sinf(angle.asRadians() * 0.5);
     const float cos_half = cosf(angle.asRadians() * 0.5);
@@ -30,6 +30,77 @@ Quat::Quat(const Vec3& axis, const Angle& angle)
     y = axis.y * sin_half;
     z = axis.z * sin_half;
     w = cos_half;
+}
+
+Quat operator+(const Quat& lhs, const Quat& rhs)
+{
+    return Quat(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w);
+}
+
+Quat operator-(const Quat& lhs, const Quat& rhs)
+{
+    return Quat(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w);
+}
+
+Quat operator*(const Quat& lhs, const Quat& rhs)
+{
+    const float nw = (lhs.w * rhs.w) - (lhs.x * rhs.x) - (lhs.y * rhs.y) - (lhs.z * rhs.z);
+    const float nx = (lhs.x * rhs.w) + (lhs.w * rhs.x) + (lhs.y * rhs.z) - (lhs.z * rhs.y);
+    const float ny = (lhs.y * rhs.w) + (lhs.w * rhs.y) + (lhs.z * rhs.x) - (lhs.x * rhs.z);
+    const float nz = (lhs.z * rhs.w) + (lhs.w * rhs.z) + (lhs.x * rhs.y) - (lhs.y * rhs.x);
+
+    return Quat(nx, ny, nz, nw);
+}
+
+Quat operator/(const Quat& lhs, const float rhs)
+{
+    return Quat(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w / rhs);
+}
+
+Quat& operator+=(Quat& lhs, const Quat& rhs)
+{
+    lhs.x += rhs.x;
+    lhs.y += rhs.y;
+    lhs.z += rhs.z;
+    lhs.w += rhs.w;
+
+    return lhs;
+}
+
+Quat& operator-=(Quat& lhs, const Quat& rhs)
+{
+    lhs.x -= rhs.x;
+    lhs.y -= rhs.y;
+    lhs.z -= rhs.z;
+    lhs.w -= rhs.w;
+
+    return lhs;
+}
+
+Quat& operator*=(Quat& lhs, const Quat& rhs)
+{
+    lhs = lhs * rhs;
+    return lhs;
+}
+
+Quat& operator*=(Quat& lhs, const float rhs)
+{
+    lhs.x *= rhs;
+    lhs.y *= rhs;
+    lhs.z *= rhs;
+    lhs.w *= rhs;
+
+    return lhs;
+}
+
+Quat& operator/=(Quat& lhs, const float rhs)
+{
+    lhs.x /= rhs;
+    lhs.y /= rhs;
+    lhs.z /= rhs;
+    lhs.w /= rhs;
+
+    return lhs;
 }
 
 float dot(const Quat& lhs, const Quat& rhs)
@@ -53,12 +124,42 @@ Quat normalise(const Quat& q)
     return Quat(q.x / l, q.y / l, q.z / l, q.w / l);
 }
 
-Quat operator*(const Quat& lhs, const Quat& rhs)
+Quat lerp(const Quat& start, const Quat& end, const float factor)
 {
-    const float nw = (lhs.w * rhs.w) - (lhs.x * rhs.x) - (lhs.y * rhs.y) - (lhs.z * rhs.z);
-    const float nx = (lhs.x * rhs.w) + (lhs.w * rhs.x) + (lhs.y * rhs.z) - (lhs.z * rhs.y);
-    const float ny = (lhs.y * rhs.w) + (lhs.w * rhs.y) + (lhs.z * rhs.x) - (lhs.x * rhs.z);
-    const float nz = (lhs.z * rhs.w) + (lhs.w * rhs.z) + (lhs.x * rhs.y) - (lhs.y * rhs.x);
+    return (start + (end - start) * factor);
+}
 
-    return Quat(nx, ny, nz, nw);
+Quat nlerp(const Quat& start, const Quat& end, const float factor)
+{
+    Quat corrected_end;
+
+    if (dot(start, end) < 0.0)
+    {
+        corrected_end = end * -1.0;
+    }
+    else
+    {
+        corrected_end = end;
+    }
+
+    return normalise(lerp(start, corrected_end, factor));
+}
+
+Quat slerp(const Quat& start, const Quat& end, const float factor)
+{
+    const float EPSILON = 1e-3;
+    const float dot_ = dot(start, end);
+
+    if (fabsf(dot_) > (1.0 - EPSILON)) 
+    {
+        return nlerp(start, end, factor);
+    }
+
+    const float theta_0 = acosf(dot_);
+    const float theta = theta_0 * factor;
+
+    Quat v2 = end - start * dot_;
+    normalise(v2);
+
+    return start * cosf(theta) + v2 * sin(theta);
 }
--- lib/math/quat.h
+++ lib/math/quat.h
@@ -1,6 +1,6 @@
 #pragma once
 
-class Vec3;
+class Quat;
 class Angle;
 
 class Quat
@@ -11,13 +11,31 @@ public:
 
     Quat(const float x, const float y, const float z, const float w = 1.0);
 
-    Quat(const Vec3& axis, const Angle& angle);
+    Quat(const Quat& axis, const Angle& angle);
 
     float x;
     float y;
     float z;
     float w;
 };
+
+Quat operator+(const Quat& lhs, const Quat& rhs);
+
+Quat operator-(const Quat& lhs, const Quat& rhs);
+
+Quat operator*(const Quat& lhs, const float rhs);
+
+Quat operator/(const Quat& lhs, const float rhs);
+
+Quat& operator+=(Quat& lhs, const Quat& rhs);
+
+Quat& operator-=(Quat& lhs, const Quat& rhs);
+
+Quat& operator*=(Quat& lhs, const Quat& rhs);
+
+Quat& operator*=(Quat& lhs, const float rhs);
+
+Quat& operator/=(Quat& lhs, const float rhs);
 
 float dot(const Quat& lhs, const Quat& rhs);
 
@@ -27,4 +45,8 @@ float length(const Quat& q);
 
 Quat normalise(const Quat& q);
 
-Quat operator*(const Quat& lhs, const Quat& rhs);
+Quat lerp(const Quat& start, const Quat& end, const float factor);
+
+Quat nlerp(const Quat& start, const Quat& end, const float factor);
+
+Quat slerp(const Quat& start, const Quat& end, const float factor);
