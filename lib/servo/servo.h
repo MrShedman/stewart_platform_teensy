5f0ac4c111c4e4230ee6de07ddc67aa4db022ba2
Ross Cunningham <roscoacer@googlemail.com>
2019-04-06 21:23:35 +0100

    Removed servo functions which are now in servo.cpp

--- lib/servo/servo.h
+++ lib/servo/servo.h
@@ -2,50 +2,17 @@
 
 #include <Arduino.h>
 
-#include "logger.h"
-
 class Servo
 {
 public:
 
-	void begin(uint8_t pin, float min, float max)
-	{
-		if (!digitalPinHasPWM(pin))
-		{
-			LOG_FATAL("Pin %d does not support PWM!", (uint32_t)pin);
-            return;
-		}
+	void begin(uint8_t pin, float min, float max);
 
-		m_pin = pin;
-		m_min = min;
-		m_max = max;
+	void setFrequency(float freq);
 
-		setFrequency(50.0);
-		setResolution(12);
+    void setResolution(uint8_t bits);
 
-		pinMode(m_pin, OUTPUT);
-		write(m_min);
-	}
-
-	void setFrequency(float freq)
-	{
-		m_frequency = freq;
-		analogWriteFrequency(m_pin, m_frequency);
-	}
-
-    void setResolution(uint8_t bits)
-    {
-        m_analog_res_bits = bits;
-        m_analog_scale = powf(2.0, (float)m_analog_res_bits);
-        analogWriteResolution(m_analog_res_bits);
-    }
-
-	void write(float usec)
-	{
-		usec = constrain(usec, m_min, m_max);
-		const uint32_t duty = (uint32_t)(usec / (1e6 / m_frequency) * m_analog_scale);
-		analogWrite(m_pin, duty);
-	}
+	void write(float usec);
 
 private:
 
