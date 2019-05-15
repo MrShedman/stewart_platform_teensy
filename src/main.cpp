5ed10dbb85d36479ed016652ffd91ee7917dd0bb
Ross Cunningham <ross.cunningham@dyson.com>
2019-05-15 11:21:08 +0100

    Move to task and scheduler system

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "pattern_manager.h"
 #include "controller.h"
 #include "rate.h"
+#include "connectivity.h"
+#include "scheduler.h"
 
 Platform platform;
 PatternManager pattern_manager;
@@ -12,14 +14,38 @@ Controller controller(platform, pattern_manager);
 
 Rate rate(100.0);
 
+void update_control(uint32_t currentTimeUs)
+{
+    controller.update();
+}
+
+Task_t control_task("control_task", nullptr, update_control, TASK_PERIOD_HZ(500), TASK_PRIORITY_REALTIME);
+
+Task_t blynk_run_task("blynk_run_task", nullptr, update_blynk, TASK_PERIOD_HZ(200), TASK_PRIORITY_HIGH);
+Task_t blynk_sync_task("blynk_sync_task", nullptr, sync_params, TASK_PERIOD_HZ(20), TASK_PRIORITY_MEDIUM_HIGH);
+
+void init_scheduler()
+{
+    schedulerInit();
+    addTask(&control_task);
+    addTask(&blynk_run_task);
+    addTask(&blynk_sync_task);
+
+    setTaskEnabled(&control_task, true);
+    setTaskEnabled(&blynk_run_task, true);
+    setTaskEnabled(&blynk_sync_task, true);
+}
+
 void setup() 
 {
+    init_blynk();
+
     platform.init_servos();
+
+    init_scheduler();
 }
 
 void loop()
 {
-    controller.update();
-
-    rate.sleep();
+    scheduler();
 }
