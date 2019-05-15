7d9baf695b025bbd35e23d748ad9b741e82fbc32
Ross Cunningham <ross.cunningham@dyson.com>
2019-05-15 14:41:52 +0100

    Added touch task

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "platform.h"
 #include "pattern_manager.h"
 #include "controller.h"
-#include "rate.h"
+#include "touch.h"
 #include "connectivity.h"
 #include "scheduler.h"
 
@@ -12,14 +12,13 @@ PatternManager pattern_manager;
 
 Controller controller(platform, pattern_manager);
 
-Rate rate(100.0);
-
 void update_control(uint32_t currentTimeUs)
 {
     controller.update();
 }
 
 Task_t control_task("control_task", nullptr, update_control, TASK_PERIOD_HZ(500), TASK_PRIORITY_REALTIME);
+Task_t touch_task("touch_task", nullptr, update_touch, TASK_PERIOD_HZ(TOUCH_FRAME_RATE), TASK_PRIORITY_REALTIME);
 
 Task_t blynk_run_task("blynk_run_task", nullptr, update_blynk, TASK_PERIOD_HZ(200), TASK_PRIORITY_HIGH);
 Task_t blynk_sync_task("blynk_sync_task", nullptr, sync_params, TASK_PERIOD_HZ(20), TASK_PRIORITY_MEDIUM_HIGH);
@@ -28,10 +27,12 @@ void init_scheduler()
 {
     schedulerInit();
     addTask(&control_task);
+    addTask(&touch_task);
     addTask(&blynk_run_task);
     addTask(&blynk_sync_task);
 
     setTaskEnabled(&control_task, true);
+    setTaskEnabled(&touch_task, true);
     setTaskEnabled(&blynk_run_task, true);
     setTaskEnabled(&blynk_sync_task, true);
 }
