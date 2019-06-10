fdb0590ac0111ba9ba50162394c4bdbf9736f4fd
Ross Cunningham <ross.cunningham@dyson.com>
2019-06-10 15:22:49 +0100

    Changes to accomodate new scheduler structure

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 #include "touch.h"
 #include "connectivity.h"
 #include "scheduler.h"
+#include "task.h"
+#include "logger_serial.h"
 
-Task_t control_task(    "control_task",     nullptr, update_control,    hertz(500.0),               TASK_PRIORITY_REALTIME);
-Task_t touch_task(      "touch_task",       nullptr, update_touch,      hertz(TOUCH_FRAME_RATE),    TASK_PRIORITY_REALTIME);
-Task_t blynk_run_task(  "blynk_run_task",   nullptr, update_blynk,      hertz(200.0),               TASK_PRIORITY_HIGH);
-Task_t blynk_sync_task( "blynk_sync_task",  nullptr, sync_params,       hertz(20.0),                TASK_PRIORITY_MEDIUM_HIGH);
+LoggerSerial logger;
+
+Task control_task(    "control_task",     nullptr, update_control,    hertz(200.0),               TASK_PRIORITY_REALTIME);
+Task touch_task(      "touch_task",       nullptr, update_touch,      hertz(TOUCH_FRAME_RATE),    TASK_PRIORITY_REALTIME);
+Task blynk_run_task(  "blynk_run_task",   nullptr, update_blynk,      hertz(200.0),               TASK_PRIORITY_HIGH);
+Task blynk_sync_task( "blynk_sync_task",  nullptr, sync_params,       hertz(20.0),                TASK_PRIORITY_MEDIUM_HIGH);
 
 void init_scheduler()
 {
@@ -21,20 +25,26 @@ void init_scheduler()
     addTask(&blynk_sync_task);
 
     setTaskEnabled(&control_task, true);
-    setTaskEnabled(&touch_task, true);
-    setTaskEnabled(&blynk_run_task, true);
-    setTaskEnabled(&blynk_sync_task, true);
+    //setTaskEnabled(&touch_task, true);
+    //setTaskEnabled(&blynk_run_task, true);
+    //setTaskEnabled(&blynk_sync_task, true);
 }
 
 void setup() 
 {
-    init_blynk();
+    while(!Serial);
 
-    init_platform();
+    LOG_INFO("Starting Stewart Platform!");
+
+    ///init_blynk();
+
+    //init_platform();
 
     init_pattern_manager();
 
     init_scheduler();
+
+    LOG_INFO("Scheduler started!");
 }
 
 void loop()
